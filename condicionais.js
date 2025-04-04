b9da729846271857ab0732ad073c0371a5013deb
Derik André <derikandrebarbosadovale@gmail.com>
2025-04-03 21:34:06 -0300

    exercicios condicionais

---
+++ condicionais.js
@@ -0,0 +1,25 @@
+let numero = prompt("digite um numero");
+
+if (numero > 0) { 
+
+    console.log("positivo."); 
+
+
+} else {
+
+    if (numero < 0) {
+
+        console.log("negativo.")
+
+
+    } else {
+
+
+            console.log("É zero.")
+
+
+    }
+}
+
+let numero = prompt("digite um numero")
+
---
+++ script.js
@@ -0,0 +1,36 @@
+alert("Hello world na tela"); 
+
+console.log("Hello world ls lo console");
+
+
+let nome = "Derik";
+const idade = 22;
+var cidade = "São Paulo";
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+console.log("meu nome é: sompilo");
+console.log(`meu nome é: ${nome}`)
+
+console.log("tenho"+ idade + "anos")
+console.log(`tenho ${idade} anos`)
+
+console.log("moro em: " + cidade); 
+console.log(`moro em: ${cidade}`)
+
+let texto = "tenho 2 celulares"
+let numero = 20; 
+
+console.log(texto);
+console.log(numero);
+
+console.log(typeof idade)
+
+let n1 = "10";
+let n2 = 10;
+
+let soma = n1 + n2;
+
+console.log(soma)
