52401cefa9bdf082be9ae080f67feacfd62ac0d9
Derik André <derikandrebarbosadovale@gmail.com>
2025-04-08 21:41:53 -0300

    java

--- repetiçoes.js
+++ repetiçoes.js
@@ -3,7 +3,7 @@
 // 2o = condição (enquanto).
 // 3o = incremento.
 
-for(let contador = 1; contador <=5; contador++) {
+/*for(let contador = 1; contador <=5; contador++) {
 
     alert(contador);
 
@@ -28,4 +28,50 @@ function exemploWhile () {
 
 } 
 
-exemploWhile()
+exemploWhile()*/
+
+
+
+/*function exercicioDeRepetição() {
+
+let numero = prompt("Digite um numero para ver a tabuada:")
+
+for (let i = 1; 1 <= 10; i++){
+     console.log('${numero} x ${i} = $')
+
+}
+
+
+console.log("fim da tabuada")
+
+}*/
+
+function exercicioDeRepetiçãoNivelIntermediario002() {
+
+let numero = prompt ("digite um numero")
+
+let numero2 = prompt ("digite o outro numero")
+
+let numero3 = prompt ("digite mais um numero")
+
+if (numero > numero2 && numero > numero3){
+
+    alert("o primeiro numero é o maior")
+
+}else{
+
+    if (numero2 > numero && numero2 > numero3)
+
+        alert("o segundo numero é o maior")
+
+}else{
+
+    if (numero3 > numero && numero3 > numero2)
+
+        alert("o terceiro é o maior")
+
+}
+
+exercicioDeRepetiçãoNivelIntermediario002()
+
+}
